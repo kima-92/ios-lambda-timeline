3c8a1840f006777dfc1606a11e2a9a5e8c5982d1
Wilma <kima_92@fastmail.com>
2019-12-03 18:11:31 -0500

    Added Playback and Record functionality to AudioCommentVC

--- LambdaTimeline/View Controllers/AudioCommentViewController.swift
+++ LambdaTimeline/View Controllers/AudioCommentViewController.swift
@@ -7,24 +7,214 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AudioCommentViewController: UIViewController {
-
+    
+    
+    // Playback
+    var audioPlayer: AVAudioPlayer?
+    
+    // Recording
+    var audioRecorder: AVAudioRecorder?
+    
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeRemainingLabel: UILabel!
+    
+    @IBOutlet weak var timeSlider: UISlider!
+    
+    private lazy var timeFormatter: DateComponentsFormatter = {
+        let formatting = DateComponentsFormatter()
+        formatting.unitsStyle = .positional // 00:00  mm:ss
+        // NOTE: DateComponentFormatter is good for minutes/hours/seconds
+        // DateComponentsFormatter not good for milliseconds, use DateFormatter instead)
+        formatting.zeroFormattingBehavior = .pad
+        formatting.allowedUnits = [.minute, .second]
+        return formatting
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //So that the numbers don't move as they change
+        timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeLabel.font.pointSize,
+                                                          weight: .regular)
+        timeRemainingLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeRemainingLabel.font.pointSize,
+                                                                   weight: .regular)
+        
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func playButtonPressed(_ sender: Any) {
+        playPause()
     }
-    */
-
+    
+    // MARK: Playback
+    
+    var timer: Timer?
+    
+    var isPlaying: Bool {
+        audioPlayer?.isPlaying ?? false
+    }
+    
+    
+    func play() {
+        audioPlayer?.play()
+        startTimer()
+        updateViews()
+    }
+    
+    private func startTimer() {
+        
+        var time = audioPlayer?.duration
+        
+        cancelTimer()
+        
+        timer = Timer.scheduledTimer(timeInterval: time ?? 1_000, target: self, selector: #selector(updateTimer(timer:)), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func updateTimer(timer: Timer) {
+        updateViews()
+    }
+    
+    func pause() {
+        audioPlayer?.pause()
+        cancelTimer()
+        updateViews()
+    }
+    
+    private func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func playPause() {
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    
+    private func updateViews() {
+        let playButtonTitle = isPlaying ? "Pause" : "Play"
+        playButton.setTitle(playButtonTitle, for: .normal)
+        
+        let elapsedTime = audioPlayer?.currentTime ?? 0
+        timeLabel.text = timeFormatter.string(from: elapsedTime)
+        
+        timeSlider.minimumValue = 0
+        timeSlider.maximumValue = Float(audioPlayer?.duration ?? 0)
+        timeSlider.value = Float(elapsedTime)
+        
+        let recordButtonTitle = isRecording ? "Stop Recording" : "Record"
+        recordButton.setTitle(recordButtonTitle, for: .normal)
+        
+    }
+    
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        recordToggle()
+    }
+    
+    // MARK: Record
+    
+    var recordURL: URL?
+    
+    var isRecording: Bool {
+        return audioRecorder?.isRecording ?? false
+    }
+    
+    func record() {
+        // Path to save in the Documents directory
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        // Filename (ISO8601 format for time) .caf
+        let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: [.withInternetDateTime])
+        
+        //2019-12-3.caf
+        let file = documentsDirectory.appendingPathComponent(name).appendingPathExtension("caf")
+        print("Record URL: \(file)")
+        
+        // 44.1 KHz  in CD quality Audio <- Audio quality
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+        // Start a recording
+        audioRecorder = try! AVAudioRecorder(url: file, format: format) //FIXME: error handling
+        
+        recordURL = file
+        audioRecorder?.delegate = self
+        audioRecorder?.record()
+        
+    }
+    
+    func stopRecording() {
+        audioRecorder?.stop()
+        audioRecorder = nil
+        updateViews()
+    }
+    
+    func recordToggle() {
+        if isRecording {
+            stopRecording()
+        } else {
+            record()
+        }
+    }
+    
+    //TODO: Play the audio after finishing a recording
+    //TODO: Know when the recording finished, so that we can play it
+    //TODO: Stop recording vs. Record button
+    
+    
 }
+
+
+extension AudioCommentViewController: AVAudioPlayerDelegate {
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let error = error {
+            print("Audio playback error: \(error)")
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        
+        updateViews() //TODO: Is this happening on the main thread?
+    }
+}
+
+extension AudioCommentViewController: AVAudioRecorderDelegate {
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        
+        if let error = error {
+            print("Record error: \(error)")
+            
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        print("Recording finished")
+        // TODO: Create player with new file URL
+        
+        if let recordURL = recordURL {
+            audioPlayer = try! AVAudioPlayer(contentsOf: recordURL) //FIXME: make safer
+        }
+        
+    }
+}
+
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
