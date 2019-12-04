838424bc4daccfb5da9fe9b36fad820e8d1be2d2
Wilma <kima_92@fastmail.com>
2019-12-04 17:04:52 -0500

    Added Recording Video Feature

--- LambdaTimeline/View Controllers/VideoPostViewController.swift
+++ LambdaTimeline/View Controllers/VideoPostViewController.swift
@@ -7,24 +7,197 @@
 //
 
 import UIKit
+import AVFoundation
 
 class VideoPostViewController: UIViewController {
-
+    
+    lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
+    var player: AVPlayer!
+    
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var cameraView: CameraPreviewView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        setUpCamera()
+        
+        // TODO: Add gesture to replay the recording
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(_:)))
+        view.addGestureRecognizer(tapGesture)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func handleTapGesture(_ tapGesture: UITapGestureRecognizer) {
+        print("tap")
+        switch(tapGesture.state) {
+            
+        case .ended:
+            playRecording()
+        default:
+            print("Handle other states: \(tapGesture.state)")
+        }
     }
-    */
-
+    
+    func playRecording() {
+        if let player = player {
+            // CMTime.zero  <- will take you to the beggining
+            player.seek(to: CMTime.zero) //CMTime(seconds: 10, preferredTimescale: 600)
+            
+            player.play()
+        }
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession.stopRunning()
+    }
+    
+    func setUpCamera() {
+        let camera = bestCamera()
+        captureSession.beginConfiguration()
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            fatalError("Can't create an input form the camera, do something better than crashing")
+        }
+        // Add inputs
+        guard captureSession.canAddInput(cameraInput) else {
+            fatalError("This session can't handle this type of input: \(cameraInput)")
+        }
+        
+        captureSession.addInput(cameraInput)
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
+        
+        // Add audio input
+        
+        let microphone = bestAudio()
+        guard let audioInput = try? AVCaptureDeviceInput(device: microphone) else {
+            fatalError("Can't create input from microphone")
+        }
+        
+        guard captureSession.canAddInput(audioInput) else {
+            fatalError("Can't add audio input")
+        }
+        
+        captureSession.addInput(audioInput)
+        // Add output
+        guard captureSession.canAddOutput(fileOutput) else {
+            fatalError("Cannot record to disk")
+        }
+        
+        captureSession.addOutput(fileOutput)
+        captureSession.commitConfiguration()
+        cameraView.session = captureSession
+    }
+    
+    private func bestAudio() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(for: .audio) {
+            return device
+        }
+        fatalError("No audio")
+    }
+    
+    
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return device
+        }
+        
+   
+        
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        fatalError("No camera on the device (or you are running on an iPhone)")
+    }
+    
+    //Recording feature:
+    
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        toggleRecord()
+    }
+    
+    private func toggleRecord() {
+        if fileOutput.isRecording {
+            fileOutput.stopRecording()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+    }
+    
+    func newRecordingURL() -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime]
+        
+        let name = formatter.string(from: Date())
+        let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
+        return fileURL
+    }
+    
+    private func updateViews() {
+        recordButton.isSelected = fileOutput.isRecording
+    }
+    
+    
+    // This will create a new vie everytime you use it, so you need to polish this
+    func playMovie(url: URL) {
+        player = AVPlayer(url: url)
+        let playerLayer = AVPlayerLayer(player: player)
+        var topRect = view.bounds
+        topRect.size.height = topRect.height / 4
+        topRect.size.width = topRect.width / 4
+        topRect.origin.y = view.layoutMargins.top
+        playerLayer.frame = topRect
+        view.layer.addSublayer(playerLayer)
+        player.play()
+    }
 }
+
+extension VideoPostViewController: AVCaptureFileOutputRecordingDelegate {
+    
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        
+        updateViews()
+    }
+    
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        
+        if let error = error {
+            print("Error saving video: \(error)")
+        }
+        //its gonna print what's in the sandbox
+        print("Video: \(outputFileURL.path)")
+        updateViews()
+        
+        playMovie(url: outputFileURL)
+    }
+    
+}
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
