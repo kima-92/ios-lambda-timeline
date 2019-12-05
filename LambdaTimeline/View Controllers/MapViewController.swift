c46991861a2645b56b96136dd551af2daff76634
Wilma <kima_92@fastmail.com>
2019-12-05 18:42:04 -0500

    Feature. Checks for permission to use location

--- LambdaTimeline/View Controllers/MapViewController.swift
+++ LambdaTimeline/View Controllers/MapViewController.swift
@@ -8,15 +8,60 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        checkLocationServices()
     }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    // Chacking if location services are even permited in the entire device
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            // setup location manager
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            // Show alert letting the user know they have tot urn this on
+        }
+    }
+    
+    //Check if WE have permission to use location in THIS app
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse :  // uses location only when the app is open  <- prefferable
+            //do map stuff
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization() // asking fro permission
+        case .restricted: // not allowed beacuse of something like parental control
+            // show alert letting know whats up
+            break
+        case .denied:
+            //show alert instructing them how to turn on permission
+            break
+        case .authorizedAlways: //  uses location even if the app is in the backgroud
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    
+    
     
 
     /*
@@ -29,4 +74,15 @@ class MapViewController: UIViewController {
     }
     */
 
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Later..
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // later
+    }
 }
